b49d414f87dd7552050027ed7254cc196c6e863d
Axenos-dev <73383848+Axenos-dev@users.noreply.github.com>
2023-02-13 10:18:23 +0200

    transformer model from paper done

--- gpt-model/model.py
+++ gpt-model/model.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 
 class MultiHeadAttention(nn.Module):
-    def __init__(self, num_heads: int, emb_dim: int):
+    def __init__(self, emb_dim: int, num_heads: int):
         super(MultiHeadAttention, self).__init__()
 
         self.emb_dim = emb_dim
@@ -18,28 +18,33 @@ class MultiHeadAttention(nn.Module):
         self.out = nn.Linear(emb_dim, emb_dim)
 
     def forward(self, values, keys, query, mask=None):
+        batch_size = query.shape[0]
+
+        value_len, key_len, query_len = values.shape[1], keys.shape[1], query.shape[1]
+
         values = self.values(values)
         keys = self.keys(keys)
         query = self.query(query)
 
-        batch_size, seq_len, _ = values.shape
+        values = values.reshape(batch_size, value_len, self.num_heads, self.head_dim)
+        keys = keys.reshape(batch_size, key_len, self.num_heads, self.head_dim)
+        query = query.reshape(batch_size, query_len, self.num_heads, self.head_dim)
 
-        values = values.view(batch_size, seq_len, self.num_heads, self.head_dim)
-        keys = keys.view(batch_size, seq_len, self.num_heads, self.head_dim)
-        query = query.view(batch_size, seq_len, self.num_heads, self.head_dim)
+        #attention = query @ keys.transpose(-1, -2) * self.head_dim**-0.5
 
-        attention = query @ keys.transpose(-2, -1) * self.head_dim**-0.5
+        attention = torch.einsum("BQHD, BKHD -> BHQK", [query, keys])
 
         if mask is not None:
             attention = attention.masked_fill(mask == 0, float('-inf'))
 
-        attention = F.softmax(attention, dim=-1)
-        attention = attention @ values
+        attention = attention ** -0.5
 
-        attention = attention.view(batch_size, seq_len, self.emb_dim)
+        attention = F.softmax(attention, dim=3)
+        attention = torch.einsum("BHQK, BVHD -> BQHD", [attention, values])
+
+        attention = attention.view(batch_size, query_len, self.emb_dim)
 
         return self.out(attention)
-
 
 class FeedForward(nn.Module):
     def __init__(self, emb_dim):
@@ -60,7 +65,7 @@ class TransformerBlock(nn.Module):
     def __init__(self, emb_dim: int, num_heads: int):
         super(TransformerBlock, self).__init__()
 
-        self.mha = MultiHeadAttention(num_heads, emb_dim)
+        self.mha = MultiHeadAttention(emb_dim=emb_dim, num_heads=num_heads)
         self.norm1 = nn.LayerNorm(emb_dim)
 
         self.feedforward = FeedForward(emb_dim)
@@ -110,14 +115,14 @@ class DecoderBlock(nn.Module):
     def __init__(self, emb_dim: int, num_heads: int):
         super(DecoderBlock, self).__init__()
         
-        self.mha = MultiHeadAttention(num_heads=num_heads, emb_dim=emb_dim)
+        self.mha = MultiHeadAttention(emb_dim=emb_dim,num_heads=num_heads)
         self.block = TransformerBlock(emb_dim, num_heads)
         
         self.norm = nn.LayerNorm(emb_dim)
         self.dropout = nn.Dropout(0.1)
         
     def forward(self, query, value, key, src_mask, trg_mask):
-        attention = self.mha(value, key, query, trg_mask)
+        attention = self.mha(query, query, query, trg_mask)
         query = self.dropout(self.norm(attention + query))
         
         out = self.block(value, key, query, src_mask)
@@ -185,19 +190,17 @@ class Transformer(nn.Module):
         # (N, 1, 1, src_len)
         return src_mask
     
+    def make_trg_mask(self, trg):
+        N, trg_len = trg.shape
+        trg_mask = torch.tril(torch.ones((trg_len, trg_len))).expand(
+            N, 1, trg_len, trg_len
+        )
+
+        return trg_mask
+    
     def forward(self, x, y):
         encoder_output = self.encoder(x)
-        out = self.decoder(y, encoder_output, self.make_src_mask(x), self.mask)
+        out = self.decoder(y, encoder_output, self.make_src_mask(x), self.make_trg_mask(y))
         
         return out
-    
-model = Transformer(
-    emb_dim=384,
-    num_heads=6,
-    num_layers=6,
-    max_length=10,
-    src_vocab_size=10,
-    trg_vocab_size=10
-)
-
-print(model(torch.tensor([[1, 2, 4, 5, 6, 7]]), torch.tensor([[1, 2, 4, 5, 6, 7, 7]])).shape)
+        
